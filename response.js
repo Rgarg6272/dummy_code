2d474318772c84462cf3f1905b5b75a0dbbfcc11
Rgarg6272 <73298337+Rgarg6272@users.noreply.github.com>
2023-09-26 15:46:49 +0530

    Create response.js

---
+++ response.js
@@ -0,0 +1,36 @@
+{
+	"get_member_delegation": [{
+		"id": "1",
+		"Capitated": "PMS",
+		"Capitated_start": "09/25/2023",
+		"Capitated_end": "09/25/2023",
+		"Dele_start_orig": "09/25/2023",
+		"Dele_start_adjust": "09/24/2023",
+		"Dele_end_orig": "09/24/2023",
+		"Dele_end_adjust": "09/24/2023"
+	}, {
+		"id": "2",
+		"Capitated": "PMS",
+		"Capitated_start": "09/26/2023",
+		"Capitated_end": "09/26/2023",
+		"Dele_start_orig": "09/26/2023",
+		"Dele_start_adjust": "09/26/2023",
+		"Dele_end_orig": "09/26/2023",
+		"Dele_end_adjust": "09/26/2023"
+	}],
+	"system": {
+		"title": "api_response",
+		"type": "object",
+		"properties": {
+			"code": {
+				"type": "integer",
+				"format": "int32",
+				"example": 200
+			},
+			"message": {
+				"type": "string",
+				"example": "Success"
+			}
+		}
+	}
+}
