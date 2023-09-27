ab6d4f5f2062966b61f09c208c298bdf717ecd5c
Rgarg6272 <73298337+Rgarg6272@users.noreply.github.com>
2023-09-27 17:03:34 +0530

    Create delegationDetails.js

---
+++ delegationDetails.js
@@ -0,0 +1,310 @@
+import { forwardRef } from "react";
+import React, { useEffect, useState } from "react";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import MaterialTable, { MTableToolbar } from "material-table";
+import SimpleSnackbar from "../../../common/AlertMessage";
+import "jspdf-autotable";
+import { TablePagination } from "@material-ui/core";
+import { useHistory } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import GetAppIcon from "@material-ui/icons/GetApp";
+import { CsvBuilder } from "filefy";
+import jsPDF from "jspdf";
+import { commonFontSizes } from "../../../css/FontSizes";
+import { DelegationTabletData } from "../../../../constants/memberData";
+import { MemberInformation } from "../MemberInformation/MemberInformation";
+import { useStyles } from "../../../css/MemberDetails";
+import { MuiPickersUtilsProvider, DatePicker, KeyboardDatePicker } from '@material-ui/pickers';
+import DateFnsUtils from '@date-io/date-fns';
+
+export const DelegationDetailsTable = ({resultData}) => {
+    const [tableData, setData] = useState(resultData);
+    const [data2, setData2] = useState([]);
+    const [snackOpen, setSnackOpen] = useState(false);
+    const [snackSev, setSnackSev] = useState("");
+    const [snackMsg, setSnackMsg] = useState("");
+    const tableRef = React.createRef();
+    const [showDialog, setshowDialog] = useState(false);
+    const [count, setCount] = useState(tableData && tableData.length > 0 ? tableData.length : 0);
+    const [memberInfo, setMemberInfo] = useState(false);
+    const classes = useStyles();
+
+    let nullObject = null;
+    let data1 = {};
+    let noData1 = "";
+    const history = useHistory();
+    const handleSnackClose = (event, reason) => {
+        if (reason === "clickaway") {
+            return;
+        }
+        setSnackOpen(false);
+    };
+
+    const handleClick = (rowData) => {
+        history.push('/MemberInformation')
+    };
+
+    const handleDateChange1 = (date, rowData) => {
+        console.log('changes',tableData.id)
+        const updatedData = tableData.map((item) =>
+            item.tableData.id === rowData.tableData.id ? { ...item, Dele_Start_Adjust: date } : item
+        );
+        setData(updatedData);
+    };
+
+    const handleDateClose1 = () => {
+    };
+
+    const handleDateChange2 = (date, rowData) => {
+        const updatedData = tableData.map((item) =>
+            item.tableData.id === rowData.tableData.id ? { ...item, Dele_End_Adjust: date } : item
+        );
+        setData(updatedData);
+    };
+
+    const handleDateClose2 = () => {
+    };
+
+
+    useEffect(() => {
+        setCount(tableData && tableData.length > 0 ? tableData.length : 0);
+    }, [resultData]);
+
+    return (
+        <div class="deleTable">
+            <MuiThemeProvider theme={theme}>
+                <div class="input">
+                    <MaterialTable
+                        key={count}
+                        class="input"
+                        localization={{
+                            body: {
+                                emptyDataSourceMessage: (
+                                    <div
+                                        style={{
+                                            color: "#A71930",
+                                            fontWeight: "bold",
+                                        }}
+                                    >
+                                    </div>
+                                ),
+                            },
+                        }}
+                        onRowClick={(event, rowData, togglePanel) =>
+                            handleClick(rowData)
+                        }
+                        autoHeight={true}
+                        icons={tableIcons}
+                        data={tableData}
+                        // loading={delegateLoading}
+                        // isLoading={delegateLoading}
+                        tableRef={tableRef}
+                        options={{
+                            paging: false,
+                            search: false,
+                            toolbar: false,
+                            sorting: false,
+                            detailPanelType: "single",
+                            selection: false,
+                            maxBodyHeight: "40vh",
+                            overflowY: "hidden !important",
+                            // exportButton: true,
+                            padding: "dense",
+                            // pageSize: 10,
+                            filtering: false,
+                            searchFieldStyle: {
+                                padding: "0px 0px 0px 10px",
+                                margin: "0px 0 0 0 ",
+                                disableUnderline: true,
+                                border: "0.5px solid #A19D9D",
+                                height: "100%",
+                                width: "18rem",
+                                borderRadius: "4px"
+                            },
+                            showTitle: false,
+                            doubleHorizontalScroll: false,
+                            headerStyle: {
+                                whiteSpace: "nowrap",
+                                position: "sticky",
+                                fontWeight: 700,
+                                fontSize: commonFontSizes.bodyTwo + "rem",
+                                color: "#2C2B2C",
+                                border: "1px solid lightgrey",
+                                textAlign: "center"
+                            },
+                            filterRowStyle: {
+                                left: "0",
+                                position: "sticky",
+                                top: 43,
+                                background: "#fff",
+                                padding: "0.3em",
+                                width: "100%",
+                                zIndex: 1,
+
+                            },
+                            cellStyle: {
+                                whiteSpace: "nowrap",
+                                fontSize: commonFontSizes.bodyTwo + "rem",
+                                zIndex: 2,
+                                cursor: "no-drop",
+                                fontWeight: 400,
+                            },
+                            rowStyle: (row) => {
+                                const id = row.tableData.id;
+                                return id % 2 === 0
+                                    ? { backgroundColor: "#F5F5F5" }
+                                    : { backgroundColor: "#fff" };
+                            },
+                        }}
+
+                        columns={[
+                            {
+                                title: "Capitated",
+                                field: "Capitated",
+                                cellStyle: {
+                                    fontSize: commonFontSizes.bodyTwo + "rem",
+                                    minWidth: 190,
+                                    maxWidth: 190,
+                                },
+                                render: (rowData) => (
+                                    <RenderValue value={rowData.Capitated ? rowData.Capitated : "-"} onClick={handleClick} />
+                                ),
+                            },
+                            {
+                                title: "Capitated Start",
+                                field: "Capitated_start",
+                                cellStyle: {
+                                    color: "#555151",
+                                    fontSize: commonFontSizes.bodyTwo + "rem",
+                                    fontWeight: 400,
+                                    minWidth: 190,
+                                    maxWidth: 190,
+                                },
+                                render: (rowData) => (
+                                    <RenderValue value={rowData.Capitated_start ? rowData.Capitated_start : "-"} />
+                                ),
+                            },
+                            {
+                                title: "Capitated End",
+                                field: "Capitated_end",
+                                cellStyle: {
+                                    color: "#555151",
+                                    fontSize: commonFontSizes.bodyTwo + "rem",
+                                    fontWeight: 400,
+                                    minWidth: 190,
+                                    maxWidth: 190,
+
+                                },
+                                render: (rowData) => (
+                                    <RenderValue value={rowData.Capitated_end ? rowData.Capitated_end : "-"} />
+                                ),
+                            },
+                            {
+                                title: "Delegable Start Original",
+                                field: "Dele_start_orig",
+                                cellStyle: {
+                                    color: "#555151",
+                                    fontSize: commonFontSizes.bodyTwo + "rem !important",
+                                    fontWeight: 400,
+                                    minWidth: 180,
+                                    maxWidth: 180,
+                                },
+                                render: (rowData) => (
+                                    <RenderValue value={rowData.Dele_start_orig ? rowData.Dele_start_orig :"-"} />
+                                ),
+                            },
+                            {
+                                title: "Delegable Start Adjusted",
+                                field: "Dele_start_adjust",
+                                cellStyle: {
+                                    color: "#555151",
+                                    fontSize: commonFontSizes.bodyTwo + "rem",
+                                    fontWeight: 400,
+                                    minWidth: 180,
+                                    maxWidth: 180,
+                                },
+                                render: (rowData) => (
+                                    <div style={{ display: "flex", alignItems: "center", justifyContent: "center", cursor: "pointer" }}>
+                                        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                                            <KeyboardDatePicker
+                                                className={classes.datePicker}
+                                                value={rowData.Dele_start_adjust ? rowData.Dele_start_adjust : "-"}
+                                                onChange={(date) => handleDateChange1(date, rowData)}
+                                                onAccept={handleDateClose1}
+                                                format="MM/dd/yyyy"
+                                                // disableFuture
+                                                openTo="date"
+                                                clearable
+                                                // inputVariant="outlined"
+                                                helperText=''
+                                                InputProps={{
+                                                    readOnly: true,
+                                                    // startAdornment: (
+                                                    //   <IconButton size="small">
+                                                    //     <EditIcon />
+                                                    //   </IconButton>
+                                                    // ),
+
+                                                    style: { width: 155, paddingLeft: "2rem" },
+                                                }}
+                                            />
+                                        </MuiPickersUtilsProvider>
+                                    </div>
+                                ),
+                            },
+                            {
+                                title: "Delegable End Original",
+                                field: "Dele_end_orig",
+                                cellStyle: {
+                                    color: "#555151",
+                                    fontSize: commonFontSizes.bodyTwo + "rem",
+                                    fontWeight: 400,
+                                    minWidth: 180,
+                                    maxWidth: 180,
+                                },
+                                render: (rowData) => (
+                                    <RenderValue value={rowData.Dele_end_orig ? rowData.Dele_end_orig : "-"} />
+                                ),
+                            },
+                            {
+                                title: "Delegable End Adjusted",
+                                field: "Dele_End_Adjust",
+                                cellStyle: {
+                                    color: "#555151",
+                                    fontSize: commonFontSizes.bodyTwo + "rem",
+                                    fontWeight: 400,
+                                    minWidth: 180,
+                                    maxWidth: 180,
+                                },
+                                render: (rowData) => (
+                                    <div style={{ display: "flex", alignItems: "center", justifyContent: "center", cursor: "pointer" }}>
+                                        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                                            <KeyboardDatePicker
+                                                className={classes.datePicker}
+                                                value={rowData.Dele_end_adjust ? rowData.Dele_end_adjust : "-"}
+                                                onChange={(date) => handleDateChange2(date, rowData)}
+                                                onAccept={handleDateClose2}
+                                                format="MM/dd/yyyy"
+                                                // disableFuture
+                                                openTo="date"
+                                                clearable
+                                                // inputVariant="outlined"
+                                                 helperText=''
+                                                InputProps={{
+                                                    readOnly: true,
+
+                                                    style: { width: 155, paddingLeft: "2rem" },
+                                                }}
+                                            />
+                                        </MuiPickersUtilsProvider>
+                                    </div>
+                                ),
+                            },
+                        ]}
+                    />
+                </div>
+            </MuiThemeProvider>
+        </div>
+    );
+};
+export default DelegationDetailsTable;
