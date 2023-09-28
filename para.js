c3b96c773075be157295e1b7551c0e770729fbb7
Rgarg6272 <73298337+Rgarg6272@users.noreply.github.com>
2023-09-28 16:29:27 +0530

    Create para.js

---
+++ para.js
@@ -0,0 +1,17 @@
+Ye Third para hai jo me likhunga usme bhi log galti nikalenge par muje kya me to bhalu hu!
+ye para amarkantak colony (Jay gurudev) me rhne wali ldki k liye jiska nam ULLU++ hai 😒
+
+ek ldki thi jo para-para krti hai 
+ek ldka tha jo meko-bo meko-bo krta rhta hai
+
+din bhar kuch kam hai nhi is ldki ko mere piche stickers waste krti rhti hai par me bhi kabhi kabhar thoda vella hota hu to reply kr deta hu fr sham ko aak bahi bakwas krti hai 
+isko lgta hai hmesha me jeet jati hu par iski ye galatfehmiyan pta nhi bhagwan kb khatm krega pr muje kya me to hmesha hara deta hu 😎 abhi kuch din phle chid gai thi kuch bat pe
+fr gussa hok rat me Good night bolk so gai fr subh aak bahi se start muje lga gussa rhegi manana pdega pr I m toh already gussa tha phel se to ulta muje manaya 
+That was my planning by the way. but hai bhot acchi isko lgta hai ki ye baccha hai pr akal se hai baccha hath pero se to noch khati h meko 😒😒 abhi kal hi chappal k piche
+pad gai pr mere se nhi jeet pai hehehe har mank chappal chorni pdi or fr apne ghar shart har gai pr abhi tk kuch khilaya nhi 
+
+But hai ye ldki bhot cute bo bhi man se dimag se soft soft hai bhot mere liye hmesa Care kregi puchegi mandir leke jaegi daily wait kregi jo aaj kal koi nhi krta kisi k liye
+par mera karti hai iska patience level zero hai fr bhi kuch to hai isk man me pta nhi ye fisal rhi h ya fisal chuki hai meko chidane ki koshis krti hai lekin bad me khud chid 
+jati hai
+
+Bass aaj itta hi likhunga ab daily daily bola na para ka to muh me mukka marunga 🫡
