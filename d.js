f3f0d612ccb155413b4c66a8490bb64686cac5eb
Rgarg6272 <73298337+Rgarg6272@users.noreply.github.com>
2023-10-25 16:40:45 +0530

    Update d.js

--- d.js
+++ d.js
@@ -1,31 +1,15 @@
-{
-                                title: "Subscriber ID",
-                                field: "SubscriberID",
-                                filtering: true,
-                                cellStyle: {
-                                    fontSize: commonFontSizes.bodyTwo + "rem",
-                                    fontWeight: 400,
-                                    minWidth: 170,
-                                    maxWidth: 170,
-                                },
-                                filterComponent: (props) => <TextField
-                                    style={{ height: "2rem" }}
-                                    type="search"
-                                    placeholder='Search'
-                                    variant="outlined"
-                                    onKeyDown={handleButtonSearch}
-                                    InputProps={{
-                                        endAdornment: (
-                                            <InputAdornment position="end">
-                                                <SearchIcon />
-                                            </InputAdornment>
-                                        )
-                                    }}
-                                    onChange={(event) => {
-                                        props.onFilterChanged(props.columnDef.tableData.id, event.target.value);
-                                    }}
-                                />,
-                                render: (rowData) => (
-                                    <RenderValue value={rowData.SubscriberID} />
-                                ),
-                            },
+ const checkInput = (updateMemberData) => {
+      //  console.log("updation",updateMemberData)
+        const memeberIdButtons = updateMemberData.some((checkInput) => {
+            if(checkInput){
+                return checkInput.value && checkInput.value.length > 0;
+            }
+        });
+        if (memeberIdButtons) {
+            setSearchBtnDisable(false);
+            setClearBtnDisable(false);
+        } else {
+            setSearchBtnDisable(true);
+            setClearBtnDisable(true);
+        }
+    };
