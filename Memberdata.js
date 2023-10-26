f8b11b63417fead2b677153f2500f855e9087b1a
Rgarg6272 <73298337+Rgarg6272@users.noreply.github.com>
2023-10-26 12:35:57 +0530

    Update Memberdata.js

--- Memberdata.js
+++ Memberdata.js
@@ -1,20 +1,68 @@
-export const NoteTypeData = [
+export const AdDelegateContactData = [
   {
-    id: "Note_type",
-    placeHolder: "Note Type",
+    id: 1,
+    placeHolder: "Enter Delegate",
     inputType: "select",
-    label: "Note Type",
-    name: "Note_type",
-    NoteTypeOptions: ["Referral Date", "Date of Care Coordinator Assignment", "Others"],
-    //"Select Note Type",
+    label: "Delegate*",
+    name: "Delegate",
+    DelegateLevelOptions: ["PMS", "PMG", "Health Home - Carelink(Clovis)"],
+    Action: "",
+    enableDatePicker: false
   },
   {
-    id: "Note_date",
-    placeHolder: "yyyy/mm/dd",
-    inputType: "date",
-    label: "",
-    name: "Note_date",
-    format: "YYYY-MM-DD",
-    checkRange: false,
+    id: 2,
+    placeHolder: "Enter Contact Type",
+    inputType: "select",
+    label: "Contact Type*",
+    name: "Contact_Type",
+    DelegateLevelOptions: ["Care Team - Delegated Care Coordinator", "Delegated Care Coordinator", "CareLink Care Coordinator"],
+    Action: "",
+    enableDatePicker: false
+  },
+  {
+    id: 3,
+    placeHolder: "First Name Last Name",
+    inputType: "text",
+    label: "Contact Name*",
+    name: "Contact_Name",
+    Action: "",
+    enableDatePicker: false
+  },
+  {
+    id: 4,
+    placeHolder: "Enter Cell Phone",
+    inputType: "text",
+    label: "Cell Phone",
+    name: "Cell_Phone",
+    Action: "",
+    enableDatePicker: false
+  },
+  {
+    id: 5,
+    placeHolder: "Enter Work Phone",
+    inputType: "text",
+    label: "Work Phone",
+    name: "Work_Phone",
+    Action: "",
+    enableDatePicker: false
+  },
+  {
+    id: 6,
+    placeHolder: "Enter Email",
+    inputType: "text",
+    label: "Email",
+    name: "Email",
+    Action: "",
+    enableDatePicker: false
+  },
+  {
+    id: 7,
+    placeHolder: "Enter Preferred Contact",
+    inputType: "select",
+    label: "Preferred Contact",
+    name: "Preferred",
+    DelegateLevelOptions: ["Cell Phone", "Work Phone"],
+    Action: "",
+    enableDatePicker: false
   },
 ];
