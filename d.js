e8b374a04d62cdbcf8dbbad64cd1b513fd67f239
Rgarg6272 <73298337+Rgarg6272@users.noreply.github.com>
2023-10-26 17:48:12 +0530

    Update d.js

--- d.js
+++ d.js
@@ -1,15 +1,33 @@
- const checkInput = (updateMemberData) => {
-      //  console.log("updation",updateMemberData)
-        const memeberIdButtons = updateMemberData.some((checkInput) => {
-            if(checkInput){
-                return checkInput.value && checkInput.value.length > 0;
+  const handleChange = (event) => {
+        if (addHeader === 'Add New Level of Care') {
+            setMemberFormData({
+                ...memberFormData,
+                [event.target.name]: event.target.value,
+            });
+        } else {
+            setMemberContactData({
+                ...memberContactData,
+                [event.target.name]: event.target.value,
+            });
+        }
+
+        const fieldValue = event.target.value;
+        const cleanedValue = fieldValue
+        const updateMemberData = delegateData.map((inputData) => {
+            if (inputData.name === event.target.name) {
+              //  console.log("input data", event.target.name, event.target.value)
+                return {
+                    ...inputData,
+                    value: length <= 10 ? event.target.value : inputData.value,
+                    hasError:(event.target.name === "Cell_Phone" || event.target.name === "Work_Phone") && event.target.value.replace(/\D/g, '').length < 10,
+                };
+            } else {
+                return {
+                    ...inputData,
+                };
             }
         });
-        if (memeberIdButtons) {
-            setSearchBtnDisable(false);
-            setClearBtnDisable(false);
-        } else {
-            setSearchBtnDisable(true);
-            setClearBtnDisable(true);
-        }
+        setDelegateInputData(updateMemberData);
+        checkInput(updateMemberData);
+
     };
