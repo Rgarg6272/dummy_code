9f749f849f4a12d64c693db0e3b7db1602675553
Rgarg6272 <73298337+Rgarg6272@users.noreply.github.com>
2023-10-27 13:12:03 +0530

    Update d.js

--- d.js
+++ d.js
@@ -1,33 +1,43 @@
-  const handleChange = (event) => {
-        if (addHeader === 'Add New Level of Care') {
-            setMemberFormData({
-                ...memberFormData,
-                [event.target.name]: event.target.value,
-            });
-        } else {
-            setMemberContactData({
-                ...memberContactData,
-                [event.target.name]: event.target.value,
-            });
+   const checkInput = (updateMemberData) => {
+        // const memeberIdButtons = updateMemberData.some((checkInput) => {
+        //     if (checkInput) {
+        //         return checkInput.value && checkInput.value.length > 0;
+        //     }
+        // });
+        //console.log(updateMemberData)
+        const isDelegateValid = updateMemberData[0].value ? updateMemberData[0].value.trim() !== "" : "";
+        const isContactTypeValid = updateMemberData[1].value ? updateMemberData[1].value.trim() !== "" : "";
+        const isContactNameValid = updateMemberData[2].value ? updateMemberData[2].value.trim() !== "" : "";
+        const isCellPhoneValid = updateMemberData[3].value ? (updateMemberData[3].value.length > 11) : "";
+        const isWorkPhoneValid = updateMemberData[4].value ? (updateMemberData[4].value.length > 11) : "";
+        const preferredContact = updateMemberData[6].value ? updateMemberData[6].value : "";
+
+        
+        let shouldEnableButton = false;
+
+        if (!preferredContact) {
+            // Preferred Contact is not selected
+            shouldEnableButton =
+                isDelegateValid &&
+                isContactTypeValid &&
+                isContactNameValid &&
+                (isCellPhoneValid || isWorkPhoneValid);
+        } else if (preferredContact === "Work Phone") {
+            // Preferred Contact is "Work Phone"
+            shouldEnableButton =
+                isDelegateValid &&
+                isContactTypeValid &&
+                isContactNameValid &&
+                isWorkPhoneValid;
+        } else if (preferredContact === "Cell Phone") {
+            // Preferred Contact is "Cell Phone"
+            shouldEnableButton =
+                isDelegateValid &&
+                isContactTypeValid &&
+                isContactNameValid &&
+                isCellPhoneValid;
         }
 
-        const fieldValue = event.target.value;
-        const cleanedValue = fieldValue
-        const updateMemberData = delegateData.map((inputData) => {
-            if (inputData.name === event.target.name) {
-              //  console.log("input data", event.target.name, event.target.value)
-                return {
-                    ...inputData,
-                    value: length <= 10 ? event.target.value : inputData.value,
-                    hasError:(event.target.name === "Cell_Phone" || event.target.name === "Work_Phone") && event.target.value.replace(/\D/g, '').length < 10,
-                };
-            } else {
-                return {
-                    ...inputData,
-                };
-            }
-        });
-        setDelegateInputData(updateMemberData);
-        checkInput(updateMemberData);
-
+        setSearchBtnDisable(!shouldEnableButton);
+        setClearBtnDisable(!shouldEnableButton);
     };
