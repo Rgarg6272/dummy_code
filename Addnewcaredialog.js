728e71670868d39523c40aec9004fcce32539394
Rgarg6272 <73298337+Rgarg6272@users.noreply.github.com>
2023-11-03 16:57:27 +0530

    Update Addnewcaredialog.js

--- Addnewcaredialog.js
+++ Addnewcaredialog.js
@@ -6,43 +6,39 @@
         const isWorkPhoneValid = updateMemberData[4].value ? (updateMemberData[4].value.length > 11) : "";
         const isCellPhone = updateMemberData[3].value ? updateMemberData[3].value : "";
         const isWorkPhone = updateMemberData[4].value ? updateMemberData[4].value : "";
-        const preferredContact = updateMemberData[6].value ? updateMemberData[6].value : "";
+        const preferredContact = updateMemberData[6].value;
         //console.log("preferredContact:",preferredContact)
+        const isPreferredContactValid = updateMemberData[6].value ? updateMemberData[6].value.trim() !== "" : "";
+        
         let shouldEnableButton = false;
         let shouldEnableClearButton = false;
-        /// console.log("preferredContact:", updateMemberData[6].value)
-        if (!preferredContact) {
-            // Preferred Contact is not selected
-            shouldEnableButton =
-                isDelegateValid &&
-                isContactTypeValid &&
-                isContactNameValid &&
-                (isCellPhoneValid || isWorkPhoneValid);
-        } else if (preferredContact === "Work Phone") {
-            // Preferred Contact is "Work Phone"
-            shouldEnableButton =
-                isDelegateValid &&
-                isContactTypeValid &&
-                isContactNameValid &&
-                isWorkPhoneValid;
-        } else if (preferredContact === "Cell Phone") {
-            // Preferred Contact is "Cell Phone"
-            shouldEnableButton =
-                isDelegateValid &&
-                isContactTypeValid &&
-                isContactNameValid &&
-                isCellPhoneValid;
-        }
-        //email validation handling
-        if (isDelegateValid && isContactTypeValid && isContactNameValid && (isCellPhoneValid || isWorkPhoneValid)) {
-            if (emailLen >= 1 && validEmail == false) {
-                shouldEnableButton = false;
-            } else if (emailLen >= 1 && validEmail == true) {
+
+        if(isDelegateValid && isContactTypeValid && isContactNameValid && isPreferredContactValid) {
+            if(preferredContact === "Cell Phone" && isCellPhoneValid){
                 shouldEnableButton = true;
-            } else if (emailLen == 0 || emailLen == 1) {
+            } else if(preferredContact === "Work Phone" && isWorkPhoneValid){
                 shouldEnableButton = true;
             }
         }
+
+       if((preferredContact === "Cell Phone" && isWorkPhoneValid) || (preferredContact === "Work Phone" && isCellPhoneValid)){
+           shouldEnableButton = false;
+       }
+
+       
+  //  email validation handling
+       
+            
+           if (isDelegateValid && isContactTypeValid && isContactNameValid && (isCellPhoneValid || isWorkPhoneValid)) { 
+              if (emailLen >= 1 && validEmail == false) {
+                 shouldEnableButton = false; 
+           } else if (emailLen >= 1 && validEmail == true) {
+                 shouldEnableButton = true;
+              } else if (emailLen == 0 || emailLen == 1) {
+                 shouldEnableButton = true;
+              }
+           }
+        
         //clear all btn handling
         if (isDelegateValid || isContactTypeValid || isContactNameValid || isCellPhone || isWorkPhone || emailLen >= 1 || preferredContact) {
             shouldEnableClearButton = true;
