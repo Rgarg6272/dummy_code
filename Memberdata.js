38de957c207e101d16e795e3ded129aaedcea4d5
Rgarg6272 <73298337+Rgarg6272@users.noreply.github.com>
2024-01-25 11:55:26 +0530

    Update Memberdata.js

--- Memberdata.js
+++ Memberdata.js
@@ -1,77 +1,22 @@
-export const AdDelegateContactData = [
+export const DashBoardDetails = [
   {
     id: 1,
-    placeHolder: "Enter Delegate",
-    inputType: "select",
-    label: "Delegate*",
-    name: "Delegate",
-    DelegateLevelOptions: ["PMS", "PMG", "Health Home - Carelink(Clovis)"],
-    Action: "",
-    enableDatePicker: false
+    label: 'MEMBER SEARCH',
+    path: MemberSearch
   },
   {
     id: 2,
-    placeHolder: "Enter Contact Type",
-    inputType: "select",
-    label: "Contact Type*",
-    name: "Contact_Type",
-    DelegateLevelOptions: ["Care Team - Delegated Care Coordinator", "Delegated Care Coordinator", "CareLink Care Coordinator"],
-    Action: "",
-    enableDatePicker: false
+    label: 'MANAGE DELEGATED CONTACTS',
+    path: ManageContacts
   },
   {
     id: 3,
-    placeHolder: "First Name Last Name",
-    inputType: "text",
-    label: "Contact Name*",
-    name: "Contact_Name",
-    Action: "",
-    enableDatePicker: false
+    label: 'MANAGE MEMBER DELEGATED CONTACTS',
+    path: MemberContacts
   },
   {
     id: 4,
-    placeHolder: "Enter Cell Phone",
-    inputType: "number",
-    label: "Cell Phone",
-    name: "Cell_Phone",
-    Action: "",
-    enableDatePicker: false,
-    maxLength: 10,
-    pattern: "\\d*",
-    errorMessage: "Should be a maximum of 10 Digit.",
-    hasError: false,
-  },
-  {
-    id: 5,
-    placeHolder: "Enter Work Phone",
-    inputType: "number",
-    label: "Work Phone",
-    name: "Work_Phone",
-    Action: "",
-    enableDatePicker: false,
-    maxLength: 10,
-    pattern: "\\d*",
-    errorMessage: "Should be a maximum of 10 Digit.",
-    hasError: false,
-  },
-  {
-    id: 6,
-    placeHolder: "Enter Email",
-    inputType: "text",
-    label: "Email",
-    name: "Email",
-    Action: "",
-    enableDatePicker: false,
-    
-  },
-  {
-    id: 7,
-    placeHolder: "Enter Preferred Contact",
-    inputType: "select",
-    label: "Preferred Contact",
-    name: "Preferred",
-    DelegateLevelOptions: ["Cell Phone", "Work Phone"],
-    Action: "",
-    enableDatePicker: false
+    label: 'ADMIN PAGE',
+    path: MemberContacts
   },
 ];
